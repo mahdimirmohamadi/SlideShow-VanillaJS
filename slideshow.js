6af1d8d605c45a0953aaa10d0e7736451c4f1717
Mahdi MirMohamadi <mahdimirmohamadi13@gmail.com>
2022-01-23 17:39:10 +0330

    JS for buttons and also (mouseover and mouseleave are handled). a gradient background is added too

--- slideshow.js
+++ slideshow.js
@@ -11,9 +11,14 @@ let goNext = () => {
 	classSwitcher()
 }
 
+let goPrev = () => {
+	active = (active == 0) ? slides.length - 1 : active - 1
+	classSwitcher()
+}
+
 //variables
 let active = 0
-let timer = 1000
+let timer = 2000
 
 let slideshow = document.querySelector(".slideshow")
 let slides = document.querySelectorAll(".slide")
@@ -22,4 +27,27 @@ let prev = document.querySelector(".prev")
 let next = document.querySelector(".next")
 
 // setInterval
-setInterval(goNext, timer)
+let runSlideshow = setInterval(goNext, timer)
+
+
+// point event
+points.forEach((point, index) => {
+    point.addEventListener("click",e =>{
+        active = index
+        classSwitcher()
+    })
+})
+
+
+// next event
+next.addEventListener("click", e => goNext())
+
+// previous event
+prev.addEventListener("click", e => goPrev())
+
+// mouseover
+
+slideshow.addEventListener("mouseover",e => clearInterval(runSlideshow))
+
+//mouseleave
+slideshow.addEventListener("mouseleave",e => runSlideshow = setInterval(goNext, timer))
